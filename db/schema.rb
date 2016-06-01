22a768c9a9c59e240849af886724ebf380948e00
Andrew Wei <andrew.wei92@yahoo.com>
2016-05-31 19:24:03 -0700

    Added api key for specific users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511012723) do
+ActiveRecord::Schema.define(version: 20160601021707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160511012723) do
     t.string   "origin"
     t.integer  "weight"
     t.boolean  "in_stock"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "api_key"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
